fc083d9c8cf82d07607ee948986bfc2f34ca83aa
rleee <danlee0150@gmail.com>
2020-02-20 20:01:07 +0800

    Connect new user, filter through users and append new user to list

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const users = [];
+io.on('connection', function(socket) {
+  console.log('Visitor ', socket.id);
+  console.log('list of users: ', users);
+
+  // Add connected users to list and emit the list out to all users
+  socket.on('user_connected', function(username) {
+    console.log('user signIn: ' + username);
+    const newUser = {
+      username: username,
+      socketID: socket.id,
+    };
+    users.push(newUser);
+    io.emit('user_connected', users);
+  });
+});
+
+http.listen(8000, () => {
+  console.log('connected at 8000');
+});
